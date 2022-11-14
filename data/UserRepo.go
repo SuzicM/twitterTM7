4ff47c43c2b7dd3aca4a7c615a766d6fc956523e
suzicm <suzicmilan15@gmail.com>
2022-11-14 11:49:21 +0100

    Login service

---
+++ data/UserRepo.go
@@ -0,0 +1,198 @@
+package data
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type UserRepo struct {
+	cli    *mongo.Client
+	logger *log.Logger
+}
+
+// NoSQL: Constructor which reads db configuration from environment
+func New(ctx context.Context, logger *log.Logger) (*UserRepo, error) {
+	dburi := os.Getenv("MONGO_DB_URI")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(dburi))
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UserRepo{
+		cli:    client,
+		logger: logger,
+	}, nil
+}
+
+func (ur *UserRepo) Disconnect(ctx context.Context) error {
+	err := ur.cli.Disconnect(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ur *UserRepo) Ping() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Check connection -> if no error, connection is established
+	err := ur.cli.Ping(ctx, readpref.Primary())
+	if err != nil {
+		ur.logger.Println(err)
+	}
+
+	// Print available databases
+	databases, err := ur.cli.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		ur.logger.Println(err)
+	}
+	fmt.Println(databases)
+}
+
+func (ur *UserRepo) getCollection() *mongo.Collection {
+	patientDatabase := ur.cli.Database("mongoDemo")
+	patientsCollection := patientDatabase.Collection("patients")
+	return patientsCollection
+}
+
+// NoSQL: Returns all products
+func (ur *UserRepo) GetAll() (Users, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	usersCollection := ur.getCollection()
+
+	var users Users
+	patientsCursor, err := usersCollection.Find(ctx, bson.M{})
+	if err != nil {
+		ur.logger.Println(err)
+		return nil, err
+	}
+	if err = patientsCursor.All(ctx, &users); err != nil {
+		ur.logger.Println(err)
+		return nil, err
+	}
+	return users, nil
+}
+
+// NoSQL: Returns Product by id
+func (ur *UserRepo) Get(id string) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	usersCollection := ur.getCollection()
+
+	var user User
+	objID, _ := primitive.ObjectIDFromHex(id)
+	err := usersCollection.FindOne(ctx, bson.M{"_id": objID}).Decode(&user)
+	if err != nil {
+		ur.logger.Println(err)
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (ur *UserRepo) LoginUser(username, password string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	usersCollection := ur.getCollection()
+
+	filterCursor, err := usersCollection.Find(ctx, bson.M{"username": username})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(filterCursor)
+}
+
+// NoSQL: Returns Product by username
+func (ur *UserRepo) GetByUsername(username string) (Users, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	usersCollection := ur.getCollection()
+
+	var users Users
+	usersCursor, err := usersCollection.Find(ctx, bson.M{"username": username})
+	if err != nil {
+		ur.logger.Println(err)
+		return nil, err
+	}
+	if err = usersCursor.All(ctx, &users); err != nil {
+		ur.logger.Println(err)
+		return nil, err
+	}
+	return users, nil
+}
+
+func (ur *UserRepo) Post(user *User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	usersCollection := ur.getCollection()
+
+	result, err := usersCollection.InsertOne(ctx, &user)
+	if err != nil {
+		ur.logger.Println(err)
+		return err
+	}
+	ur.logger.Printf("Documents ID: %v\n", result.InsertedID)
+	return nil
+}
+
+func (ur *UserRepo) Put(id string, user *User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	usersCollection := ur.getCollection()
+
+	objID, _ := primitive.ObjectIDFromHex(id)
+	filter := bson.M{"_id": objID}
+	update := bson.M{"$set": bson.M{
+		"name":      user.Name,
+		"surname":   user.Surname,
+		"username":  user.Username,
+		"password":  user.Password,
+		"age":       user.Age,
+		"gender":    user.Gender,
+		"residance": user.Residance,
+	}}
+	result, err := usersCollection.UpdateOne(ctx, filter, update)
+	ur.logger.Printf("Documents matched: %v\n", result.MatchedCount)
+	ur.logger.Printf("Documents updated: %v\n", result.ModifiedCount)
+
+	if err != nil {
+		ur.logger.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (ur *UserRepo) Delete(id string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	usersCollection := ur.getCollection()
+
+	objID, _ := primitive.ObjectIDFromHex(id)
+	filter := bson.D{{Key: "_id", Value: objID}}
+	result, err := usersCollection.DeleteOne(ctx, filter)
+	if err != nil {
+		ur.logger.Println(err)
+		return err
+	}
+	ur.logger.Printf("Documents deleted: %v\n", result.DeletedCount)
+	return nil
+}
---
+++ data/user.go
@@ -0,0 +1,37 @@
+package data
+
+import (
+	"encoding/json"
+	"io"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Defining the main struct for our API
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name      string             `bson:"name" json:"name" validate:"required"`
+	Surname   string             `bson:"surname" json:"surname" validate:"required"`
+	Username  string             `bson:"username" json:"username" validate:"required"`
+	Password  string             `bson:"password" json:"password" validate:"required"`
+	Age       string             `bson:"age" json:"age" validate:"required"`
+	Gender    string             `bson:"gender" json:"gender" validate:"required"`
+	Residance string             `bson:"residance" json:"residance" validate:"required"`
+}
+
+type Users []*User
+
+func (p *Users) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func (p *User) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func (p *User) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(p)
+}
